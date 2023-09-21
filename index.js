18cb9e265196bd6872935e4acbbe1822bef8a6ea
weiwei.huang <weiwei.huang@brtchip.com>
2023-09-21 12:37:34 +0800

    Using ESModules

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const { CommonJSModulesExample } = require('./modules/commonjs');
-const CommonJSModules = require('./modules/commonjs');
+// CommonJS Modules
+// const { CommonJSModulesExample } = require('./modules/commonjs');
+// const CommonJSModules = require('./modules/commonjs');
 
-CommonJSModulesExample();
-CommonJSModules.CommonJSModulesExample();
+// CommonJSModulesExample();
+// CommonJSModules.CommonJSModulesExample();
+
+// ESModules
+import { ESModulesExample } from "./modules/esmodules.js";
+import constantExpression from "./modules/esmodules.js";
+import * as ESModules from "./modules/esmodules.js";
+
+ESModulesExample();
+console.log(constantExpression);
+ESModules.ESModulesExample();
---
+++ modules/esmodules.js
@@ -0,0 +1,8 @@
+const ESMODULE = 'ESModule';
+
+const ESModulesExample = () => {
+    console.log(`${ESMODULE} Export`);
+}
+
+export default ESMODULE;
+export { ESModulesExample };
